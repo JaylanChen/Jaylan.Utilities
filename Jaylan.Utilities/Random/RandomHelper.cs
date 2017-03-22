caf25413ea2f53daeebcef732f0b46c13afe994a
Jaylan <chen.jinliang@tfchina.org>
2017-03-22 13:58:42 +0800

    random & string

---
+++ Jaylan.Utilities/Random/RandomHelper.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jaylan.Utilities.Random
+{
+    /// <summary>
+    /// 使用Random类生成伪随机数
+    /// </summary>
+    public class RandomHelper
+    {
+        //随机数对象
+        private static readonly System.Random HelperRandom = new System.Random(~unchecked((int)DateTime.Now.Ticks));
+
+        #region 对一个数组进行随机排序
+        /// <summary>
+        /// 对一个数组进行随机排序
+        /// </summary>
+        /// <typeparam name="T">数组的类型</typeparam>
+        /// <param name="arr">需要随机排序的数组</param>
+        public static void GetRandomArray<T>(T[] arr)
+        {
+            //对数组进行随机排序的算法:随机选择两个位置，将两个位置上的值交换
+
+            //交换的次数,这里使用数组的长度作为交换次数
+            var count = arr.Length;
+
+            //开始交换
+            for (var i = 0; i < count; i++)
+            {
+                //生成两个随机数位置
+                var randomNum1 = GetRandomInt(0, arr.Length);
+                var randomNum2 = GetRandomInt(0, arr.Length);
+
+                //定义临时变量
+
+                //交换两个随机数位置的值
+                var temp = arr[randomNum1];
+                arr[randomNum1] = arr[randomNum2];
+                arr[randomNum2] = temp;
+            }
+        }
+
+        /// <summary>
+        /// 随机生成不重复数字字符串
+        /// </summary>
+        /// <param name="codeCount">字符串长度</param>
+        /// <returns></returns>
+        public static string GenerateCheckCodeNum(int codeCount)
+        {
+            var rep = 0;
+            var str = string.Empty;
+            var num2 = DateTime.Now.Ticks + rep;
+            rep++;
+            var random = new System.Random(((int)(((ulong)num2) & 0xffffffffL)) | ((int)(num2 >> rep)));
+            for (var i = 0; i < codeCount; i++)
+            {
+                var num = random.Next();
+                str = str + ((char)(0x30 + ((ushort)(num % 10)))).ToString();
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// 随机生成字符串（数字和字母混和）
+        /// </summary>
+        /// <param name="codeCount">字符串长度</param>
+        /// <returns></returns>
+        public static string GenerateCheckCode(int codeCount)
+        {
+            var rep = 0;
+            var str = string.Empty;
+            var num2 = DateTime.Now.Ticks + rep;
+            rep++;
+            var random = new System.Random(((int)(((ulong)num2) & 0xffffffffL)) | ((int)(num2 >> rep)));
+            for (var i = 0; i < codeCount; i++)
+            {
+                char ch;
+                var num = random.Next();
+                if ((num % 2) == 0)
+                {
+                    ch = (char)(0x30 + ((ushort)(num % 10)));
+                }
+                else
+                {
+                    ch = (char)(0x41 + ((ushort)(num % 0x1a)));
+                }
+                str = str + ch.ToString();
+            }
+            return str;
+        }
+        #endregion
+
+        #region 产生随机整数
+        /// <summary>
+        /// 产生随机数，100000~999999之间
+        /// 包含100000不包含999999
+        /// </summary>
+        /// <returns>随机数</returns>
+        public static int GetRandomInt()
+        {
+            const int minimum = 100000;
+            const int maximal = 999999;
+            return GetRandomInt(minimum, maximal);
+        }
+
+        /// <summary>
+        /// 生成一个指定范围的随机整数，该随机数范围包括最小值，但不包括最大值
+        /// </summary>
+        /// <param name="minNum">最小值</param>
+        /// <param name="maxNum">最大值</param>
+        public static int GetRandomInt(int minNum, int maxNum)
+        {
+            return HelperRandom.Next(minNum, maxNum);
+        }
+
+        /// <summary>
+        /// 生成一个指定范围的随机整数List，该随机数范围包括最小值，但不包括最大值
+        /// </summary>
+        /// <param name="count">List 大小</param>
+        /// <param name="minNum">最小值</param>
+        /// <param name="maxNum">最大值</param>
+        public static IEnumerable<int> GetRandomIntList(int count, int minNum, int maxNum)
+        {
+            var numList = new List<int>();
+            while (numList.Count < count)
+            {
+                var num = GetRandomInt(minNum, maxNum);
+                if (!numList.Contains(num))
+                {
+                    numList.Add(num);
+                }
+            }
+            return numList;
+        }
+
+        #endregion
+
+        #region 生成一个0.0到1.0的随机小数
+        /// <summary>
+        /// 生成一个0.0到1.0的随机小数
+        /// </summary>
+        public static double GetRandomDouble()
+        {
+            return HelperRandom.NextDouble();
+        }
+        #endregion
+
+        #region 生成数字的随机字符串
+        /// <summary>
+        /// 生成数字的随机字符串
+        /// </summary>
+        /// <param name="length">生成长度</param>
+        public static string GetNumberString(int length)
+        {
+            return GetNumberString(length, false);
+        }
+
+        /// <summary>
+        /// 生成数字的随机字符串
+        /// </summary>
+        /// <param name="length">生成长度</param>
+        /// <param name="sleep">是否要在生成前将当前线程阻止以避免重复</param>
+        public static string GetNumberString(int length, bool sleep)
+        {
+            if (sleep) System.Threading.Thread.Sleep(5);
+            var result = "";
+            for (var i = 0; i < length; i++)
+            {
+                result += HelperRandom.Next(10).ToString();
+            }
+            return result;
+        }
+        #endregion
+
+        #region 生成字母与数字的随机字符串
+        /// <summary>
+        /// 生成字母与数字的随机字符串
+        /// </summary>
+        /// <param name="length">生成长度</param>
+        public static string GetString(int length)
+        {
+            return GetString(length, false);
+        }
+
+        /// <summary>
+        /// 生成字母与数字的随机字符串
+        /// </summary>
+        /// <param name="length">生成长度</param>
+        /// <param name="sleep">是否要在生成前将当前线程阻止以避免重复</param>
+        public static string GetString(int length, bool sleep)
+        {
+            if (sleep) System.Threading.Thread.Sleep(3);
+            var pattern = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            var result = "";
+            var n = pattern.Length;
+            for (var i = 0; i < length; i++)
+            {
+                var rnd = HelperRandom.Next(0, n);
+                result += pattern[rnd];
+            }
+            return result;
+        }
+        #endregion
+
+        #region 生成纯字母的随机字符串
+        /// <summary>
+        /// 生成纯字母的随机字符串
+        /// </summary>
+        /// <param name="length">生成长度</param>
+        public static string GetCharString(int length)
+        {
+            return GetCharString(length, false);
+        }
+
+        /// <summary>
+        /// 生成纯字母的随机字符串
+        /// </summary>
+        /// <param name="length">生成长度</param>
+        /// <param name="sleep">是否要在生成前将当前线程阻止以避免重复</param>
+        public static string GetCharString(int length, bool sleep)
+        {
+            if (sleep) System.Threading.Thread.Sleep(3);
+            var pattern = new[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            var result = "";
+            var n = pattern.Length;
+            for (var i = 0; i < length; i++)
+            {
+                var rnd = HelperRandom.Next(0, n);
+                result += pattern[rnd];
+            }
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ Jaylan.Utilities/String/StringHelper.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Jaylan.Utilities.String
+{
+    /// <summary>
+    /// 字符串拓展类
+    /// </summary>
+    public static class StringHelper
+    {
+        #region 字符串验证
+        /// <summary>
+        /// 是否是邮箱地址
+        /// </summary>
+        /// <param name="email">待检测的字符串</param>
+        /// <returns></returns>
+        public static bool IsMail(this string email)
+        {
+            return QuickValidate(@"^[\w\-]+?@[\w\-]+\.[\w\-]+$", email) && email.IndexOf('@') == email.LastIndexOf('@');
+        }
+
+        /// <summary>
+        /// 是否是手机号
+        /// </summary>
+        /// <param name="mobile">待检测的字符串</param>
+        /// <returns></returns>
+        public static bool IsMobile(this string mobile)
+        {
+            return QuickValidate(@"^1[3578]\d{9}$", mobile);
+        }
+
+        /// <summary>
+        /// 是否是数字字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNumeric(this string str)
+        {
+            return QuickValidate(@"^\d+$", str);
+        }
+
+        /// <summary>
+        /// 判断字符串是否是Null 或者 空字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+        #endregion
+
+        #region 字符串截取
+        /// <summary>
+        /// 从指定位置截取字符串，如果起始位置超出了字符串长度则返回空字符串
+        /// </summary>
+        /// <param name="str">带截取的字符串</param>
+        /// <param name="start">截取的开始位置(从0开始)</param>
+        /// <returns>返回截取的字符串</returns>
+        public static string SubStr(this string str, int start)
+        {
+            if (str == null)
+                throw new ArgumentNullException("str");
+            var value = string.Empty;
+            if (str.Length > start)
+            {
+                value = value.Substring(start);
+            }
+            return value;
+        }
+        /// <summary>
+        /// 从指定的位置截取指定长度的字符串
+        /// </summary>
+        /// <param name="str">带截取的字符串</param>
+        /// <param name="start">截取的开始位置(从0开始)</param>
+        /// <param name="len">截取的结束位置(从0开始)</param>
+        /// <returns>返回截取的字符串</returns>
+        public static string SubStr(this string str, int start, int len)
+        {
+            var value = str.SubStr(start);
+            if (value.Length > len)
+                value = value.Substring(0, len);
+            return value;
+        }
+        #endregion
+
+        #region 字符串分割
+        /// <summary>
+        /// 把字符串按照分隔符转换成 List
+        /// </summary>
+        /// <param name="str">源字符串</param>
+        /// <param name="speater">分隔符</param>
+        /// <param name="toLower">是否转换为小写</param>
+        /// <returns></returns>
+        public static List<string> GetStrArray(string str, char speater, bool toLower)
+        {
+            var list = new List<string>();
+            var ss = str.Split(speater);
+            foreach (var s in ss)
+            {
+                if (!string.IsNullOrEmpty(s) && s != speater.ToString())
+                {
+                    var strVal = s;
+                    if (toLower)
+                    {
+                        strVal = s.ToLower();
+                    }
+                    list.Add(strVal);
+                }
+            }
+            return list;
+        }
+        #endregion
+
+        #region 字符编码转换
+        /// <summary>
+        /// 将一个字符串从一个编码转换为另一个编码
+        /// </summary>
+        /// <param name="str">待处理字符串</param>
+        /// <param name="lastEncoding">转换前编码</param>
+        /// <param name="nextEncoding">转换后编码</param>
+        /// <returns></returns>
+        public static string ChangeEn(string str, Encoding lastEncoding, Encoding nextEncoding)
+        {
+            var byteStr = lastEncoding.GetBytes(str);
+            byteStr = Encoding.Convert(lastEncoding, nextEncoding, byteStr);
+            var newStr = nextEncoding.GetString(byteStr);
+            return newStr;
+        }
+        #endregion
+
+        #region 全角半角转换
+        /// <summary>
+        /// 转全角的函数(SBC case)
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToSBC(string str)
+        {
+            //半角转全角：
+            var c = str.ToCharArray();
+            for (var i = 0; i < c.Length; i++)
+            {
+                if (c[i] == 32)
+                {
+                    c[i] = (char)12288;
+                    continue;
+                }
+                if (c[i] < 127)
+                    c[i] = (char)(c[i] + 65248);
+            }
+            return new string(c);
+        }
+
+        /// <summary>
+        ///  转半角的函数(SBC case)
+        /// </summary>
+        /// <param name="str">输入</param>
+        /// <returns></returns>
+        public static string ToDBC(string str)
+        {
+            var c = str.ToCharArray();
+            for (var i = 0; i < c.Length; i++)
+            {
+                if (c[i] == 12288)
+                {
+                    c[i] = (char)32;
+                    continue;
+                }
+                if (c[i] > 65280 && c[i] < 65375)
+                    c[i] = (char)(c[i] - 65248);
+            }
+            return new string(c);
+        }
+        #endregion
+
+        #region HTML转行成TEXT
+        /// <summary>
+        /// HTML转行成TEXT
+        /// </summary>
+        /// <param name="strHtml"></param>
+        /// <returns></returns>
+        public static string HtmlToTxt(string strHtml)
+        {
+            string[] aryReg ={
+            @"<script[^>]*?>.*?</script>",
+            @"<(\/\s*)?!?((\w+:)?\w+)(\w+(\s*=?\s*(([""'])(\\[""'tbnr]|[^\7])*?\7|\w+)|.{0})|\s)*?(\/\s*)?>",
+            @"([\r\n])[\s]+",
+            @"&(quot|#34);",
+            @"&(amp|#38);",
+            @"&(lt|#60);",
+            @"&(gt|#62);",
+            @"&(nbsp|#160);",
+            @"&(iexcl|#161);",
+            @"&(cent|#162);",
+            @"&(pound|#163);",
+            @"&(copy|#169);",
+            @"&#(\d+);",
+            @"-->",
+            @"<!--.*\n"
+            };
+            //string strOutput = strHtml;
+            //for (int i = 0; i < aryReg.Length; i++)
+            //{
+            //    Regex regex = new Regex(aryReg[i], RegexOptions.IgnoreCase);
+            //    strOutput = regex.Replace(strOutput, string.Empty);
+            //}
+            var strOutput = aryReg.Select(t => new Regex(t, RegexOptions.IgnoreCase)).Aggregate(strHtml, (current, regex) => regex.Replace(current, string.Empty));
+
+            strOutput = strOutput.Replace("<", "");
+            strOutput = strOutput.Replace(">", "");
+            strOutput = strOutput.Replace("\r\n", "");
+
+
+            return strOutput;
+        }
+        #endregion
+
+        #region 得到字符串字节长度
+        /// <summary>
+        /// 得到字符串字节长度
+        /// </summary>
+        /// <param name="str">参数字符串</param>
+        /// <param name="encoding">以什么编码计算</param>
+        /// <returns></returns>
+        public static int StrLength(string str, Encoding encoding)
+        {
+            var tempLen = 0;
+            var s = encoding.GetBytes(str);
+            foreach (var b in s)
+            {
+                if ((int)b == 63)
+                    tempLen += 2;
+                else
+                    tempLen += 1;
+            }
+            return tempLen;
+        }
+        #endregion
+
+        #region SQL语句字符串安全处理
+        /// <summary>
+        /// SQL语句字符串安全处理，防止非法注入
+        /// </summary>
+        /// <param name="String">字符串</param>
+        /// <param name="isDel">是删除还是替换，true表示删除，false表示转换</param>
+        /// <returns></returns>
+        public static string SqlSafeString(string String, bool isDel)
+        {
+            if (isDel)
+            {
+                String = String.Replace("'", "");
+                String = String.Replace("\"", "");
+                return String;
+            }
+            String = String.Replace("'", "&#39;");
+            String = String.Replace("\"", "&#34;");
+            return String;
+        }
+        #endregion
+
+        #region 检查一个字符串是否是纯数字构成的，一般用于查询字符串参数的有效性验证。
+        /// <summary>
+        /// 检查一个字符串是否是纯数字构成的，一般用于查询字符串参数的有效性验证。(0除外)
+        /// </summary>
+        /// <param name="value">需验证的字符串。。</param>
+        /// <returns>是否合法的bool值。</returns>
+        public static bool IsNumberId(string value)
+        {
+            return QuickValidate("^[1-9]*[0-9]*$", value);
+        }
+        #endregion
+
+        #region 快速验证一个字符串是否符合指定的正则表达式。
+        /// <summary>
+        /// 快速验证一个字符串是否符合指定的正则表达式。
+        /// </summary>
+        /// <param name="express">正则表达式的内容。</param>
+        /// <param name="value">需验证的字符串。</param>
+        /// <returns>是否合法的bool值。</returns>
+        public static bool QuickValidate(string express, string value)
+        {
+            if (value == null)
+                return false;
+            var myRegex = new Regex(express);
+            if (value.Length == 0)
+            {
+                return false;
+            }
+            return myRegex.IsMatch(value);
+        }
+        #endregion
+    }
+}
