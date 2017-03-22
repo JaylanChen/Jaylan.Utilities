cd6cc6ff55ca2d07bf11dd32b5dbbf755302b310
Jaylan <chen.jinliang@tfchina.org>
2017-03-22 13:36:25 +0800

    http

---
+++ Jaylan.Utilities/Http/HttpRequsetHelper.cs
@@ -0,0 +1,71 @@
+﻿using System.Web;
+
+namespace Jaylan.Utilities.Http
+{
+    public class HttpRequsetHelper
+    {
+        /// <summary>
+        /// 获取客户端IP地址
+        /// </summary>
+        /// <returns>若失败则返回127.0.0.1</returns>
+        public static string GetHostAddress()
+        {
+            var request = HttpContext.Current.Request;
+            string userHostAddress=null;
+            //如果客户端使用了代理服务器，则利用HTTP_X_FORWARDED_FOR找到客户端IP地址
+            var xff = request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (!string.IsNullOrEmpty(xff))
+            {
+                userHostAddress = xff.Split(',')[0].Trim();
+            }
+            //否则直接读取REMOTE_ADDR获取客户端IP地址
+            if (string.IsNullOrEmpty(userHostAddress))
+            {
+                userHostAddress = request.ServerVariables["REMOTE_ADDR"];
+            }
+            //前两者均失败，则利用Request.UserHostAddress属性获取IP地址，但此时无法确定该IP是客户端IP还是代理IP
+            if (string.IsNullOrEmpty(userHostAddress))
+            {
+                userHostAddress = request.UserHostAddress;
+            }
+            //最后判断获取是否成功，并检查IP地址的格式（检查其格式非常重要）
+            if (string.IsNullOrEmpty(userHostAddress) || !IsIP(userHostAddress))
+            {
+                userHostAddress = "127.0.0.1";
+            }
+            return userHostAddress;
+        }
+
+
+        /// <summary>
+        /// 获取客户端IP地址（无视代理）
+        /// </summary>
+        /// <returns>若失败则返回127.0.0.1</returns>
+        public static string GetHostAddressIgnoreProxy()
+        {
+            var request = HttpContext.Current.Request;
+            var userHostAddress = request.UserHostAddress;
+
+            if (string.IsNullOrEmpty(userHostAddress))
+            {
+                userHostAddress = request.ServerVariables["REMOTE_ADDR"];
+            }
+            //最后判断获取是否成功，并检查IP地址的格式（检查其格式非常重要）
+            if (!string.IsNullOrEmpty(userHostAddress) && IsIP(userHostAddress))
+            {
+                return userHostAddress;
+            }
+            return "127.0.0.1";
+        }
+
+        /// <summary>
+        /// 检查IP地址格式
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        public static bool IsIP(string ip)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$");
+        }
+    }
+}
