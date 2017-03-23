604768efbfc48467f3d05d5f007f3dfcac94b9e8
Jaylan <chen.jinliang@tfchina.org>
2017-03-23 16:31:44 +0800

    lambda 动态拼接

--- Jaylan.Utilities/LinqExtension.cs
+++ Jaylan.Utilities/LinqExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Jaylan.Utilities
 {
@@ -17,10 +18,71 @@ namespace Jaylan.Utilities
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
         {
             var hash = new HashSet<TKey>();
+            var result = from p in source
+                         where hash.Add(keySelector(p))
+                         select p;
+            return result;
+        }
 
-            return (from p in source
-                    where hash.Add(keySelector(p))
-                    select p);
+
+        public static Expression<Func<T, bool>> True<T>() { return f => true; }
+
+        public static Expression<Func<T, bool>> False<T>() { return f => false; }
+
+        /// <summary>
+        /// 拼接Lambda表达式 
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="first">第一个表达式</param>
+        /// <param name="second">第二个表达式</param>
+        /// <param name="merge">连接类型</param>
+        /// <returns></returns>
+        public static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
+        {
+            // build parameter map (from parameters of second to parameters of first)
+            var map = first.Parameters.Select((f, i) => new { f, s = second.Parameters[i] }).ToDictionary(p => p.s, p => p.f);
+
+            // replace parameters in the second lambda expression with parameters from the first
+            var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
+
+            // apply composition of lambda expression bodies to parameters from the first expression
+            return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.And);
+        }
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.Or);
+        }
+    }
+
+
+    public class ParameterRebinder : ExpressionVisitor
+    {
+        private readonly Dictionary<ParameterExpression, ParameterExpression> _map;
+
+        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+        {
+            _map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+        }
+
+        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+        {
+            return new ParameterRebinder(map).Visit(exp);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression p)
+        {
+            ParameterExpression replacement;
+            if (_map.TryGetValue(p, out replacement))
+            {
+                p = replacement;
+            }
+            return base.VisitParameter(p);
         }
     }
 }
