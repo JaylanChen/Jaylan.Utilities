e30416c51545e0f4a6313ef9bfe6c7198a7fbdab
Jaylan <chen.jinliang@tfchina.org>
2017-03-27 10:08:41 +0800

    add WebApi

---
+++ Jaylan.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Jaylan.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Jaylan.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jaylan.WebApi.WebApiApplication" Language="C#" %>
