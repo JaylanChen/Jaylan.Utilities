e3327f126405738281198fc36df9966cca6f53f7
Jaylan <chen.jinliang@tfchina.org>
2017-03-27 11:33:04 +0800

    remove server

---
+++ Jaylan.WebApi/HttpModules/CustomHeaderModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+
+namespace Jaylan.WebApi.HttpModules
+{
+    public class CustomHeaderModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        private static void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            //HttpContext.Current.Response.Headers.Remove("Server");
+            HttpContext.Current.Response.Headers.Set("Server", "Jaylan Server");
+        }
+    }
+}
