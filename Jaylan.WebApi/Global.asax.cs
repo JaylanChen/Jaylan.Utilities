148d8de454249195df081d88d7da25a084a3a038
Jaylan <chen.jinliang@tfchina.org>
2017-03-27 11:07:03 +0800

    只查找.cshtml视图

--- Jaylan.WebApi/Global.asax.cs
+++ Jaylan.WebApi/Global.asax.cs
@@ -13,6 +13,21 @@ namespace Jaylan.WebApi
     {
         protected void Application_Start()
         {
+
+            #region 只查找.cshtml视图
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine()
+            {
+                AreaViewLocationFormats = new string[] { "~/Areas/{2}/Views/{1}/{0}.cshtml", "~/Areas/{2}/Views/Shared/{0}.cshtml" },
+                AreaMasterLocationFormats = new string[] { "~/Areas/{2}/Views/{1}/{0}.cshtml", "~/Areas/{2}/Views/Shared/{0}.cshtml" },
+                AreaPartialViewLocationFormats = new string[] { "~/Areas/{2}/Views/{1}/{0}.cshtml", "~/Areas/{2}/Views/Shared/{0}.cshtml" },
+                ViewLocationFormats = new string[] { "~/Views/{1}/{0}.cshtml", "~/Views/Shared/{0}.cshtml" },
+                MasterLocationFormats = new string[] { "~/Views/{1}/{0}.cshtml", "~/Views/Shared/{0}.cshtml" },
+                PartialViewLocationFormats = new string[] { "~/Views/{1}/{0}.cshtml", "~/Views/Shared/{0}.cshtml" },
+                FileExtensions = new string[] { "cshtml" }
+            });
+            #endregion
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
